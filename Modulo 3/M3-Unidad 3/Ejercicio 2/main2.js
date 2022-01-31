a4d82c06679d9734dc5fe5dd622726c2cfe0c1ea
JUAN AUGUSTO FELIX <juan11235felix@gmail.com>
2022-01-31 20:28:44 -0300

    tarea

--- Modulo 3/M3-Unidad 3/Ejercicio 2/main2.js
+++ Modulo 3/M3-Unidad 3/Ejercicio 2/main2.js
@@ -1,20 +1,2 @@
 /* ejercicio 2
 Crear el código que reciba como parámetro un array de números y devuelva el mayor. */
-
-var x = 1;
-var c = [];
-var mayor;
-while (x != "") {
-    var a = prompt("Ingrese numero - Deje en blanco para obtener el mayor");
-    x = parseInt(a);
-    c.push(x);
-
-};
-/*
-for (let i = 0; i < c.length; i++) {
-    if (c[i] > mayor) {
-        mayor = c[i]
-    }
-}
-*/
-document.write(c)
