9308caba14dd622233074ec3bfe575c29382f9f1
juan11235felix <juan11235felix@gmail.com>
2022-02-15 17:47:04 -0300

    react

---
+++ Modulo 4/frontend/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <footer>
+            <p>Derechos reservados. Transportes X Año</p>
+        </footer>
+    );
+}
+
+export default Footer;
