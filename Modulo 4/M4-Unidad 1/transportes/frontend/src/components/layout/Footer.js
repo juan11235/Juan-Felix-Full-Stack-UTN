2375a40ff4bf47252d568f04077a14d0fb7d6ac4
juan11235felix <juan11235felix@gmail.com>
2022-02-20 17:23:10 -0300

    tarea react 1

---
+++ Modulo 4/M4-Unidad 1/transportes/frontend/src/components/layout/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return (
+        <footer>
+            <p>Derechos Reservados. Transportes X año</p>
+        </footer>
+    );
+}
+
+export default Footer;
