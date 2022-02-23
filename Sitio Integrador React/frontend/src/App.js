f5c0304e264572877301d4c561c62ffd81f3674c
juan11235felix <juan11235felix@gmail.com>
2022-02-23 17:57:27 -0300

    tarea

--- Sitio Integrador React/frontend/src/App.js
+++ Sitio Integrador React/frontend/src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
+
+import Landing from './components/layout/Landing';
+
+import Footer from './components/layout/Footer';
+import Header from './components/layout/Header';
+import Nav from './components/layout/Nav';
+
+import HomePage from './pages/HomePage';
+import ContactoPage from './pages/ContactoPage';
+import NosotrosPage from './pages/NosotrosPage';
+import ServiciosPage from './pages/ServiciosPage'
 
 function App() {
   return (
+    <div>
+      <Landing />
+      <Header />
+      <Nav />
+      <Footer />
+    </div>
+      
+    
    
   );
 }
---
+++ Sitio Integrador React/frontend/src/components/layout/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = (props) => {
+    return (
+        <div>
+            <div>Landing</div>
+            <button>Enter</button>
+        </div>
+    );
+}
+
+export default Landing
