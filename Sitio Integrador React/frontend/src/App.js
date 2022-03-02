ca94f599fe05a22eda97bd2bb2340725a6fa21f2
juan11235felix <juan11235felix@gmail.com>
2022-03-02 15:29:19 -0300

    sitio react

--- Sitio Integrador React/frontend/src/App.js
+++ Sitio Integrador React/frontend/src/App.js
@@ -5,22 +5,11 @@ import './App.css';
 
 import Landing from './components/layout/Landing';
 
-import Footer from './components/layout/Footer';
-import Header from './components/layout/Header';
-import Nav from './components/layout/Nav';
-
-import HomePage from './pages/HomePage';
-import ContactoPage from './pages/ContactoPage';
-import NosotrosPage from './pages/NosotrosPage';
-import ServiciosPage from './pages/ServiciosPage'
-
 function App() {
   return (
     <div>
       <Landing />
-      <Header />
-      <Nav />
-      <Footer />
+      
     </div>
       
     
--- Sitio Integrador React/frontend/src/components/layout/Landing.js
+++ Sitio Integrador React/frontend/src/components/layout/Landing.js
@@ -1,10 +1,36 @@
 import React from "react";
+import '../../styles/components/layout/Landing.css'
 
 const Landing = (props) => {
     return (
         <div>
-            <div>Landing</div>
-            <button>Enter</button>
+            <div class="todo">
+                <img class="todo-fondo" src="./Images/espresso.jpg" alt="Cafe Espresso"/>
+            </div>
+
+
+            <div class="todo-menos-img">
+
+                <div class="logo-titulo">
+                    <div class="logo">
+                        <a href="#" target="_blank">
+                            <img src="./Images/favicon.ico" alt="Coffee Shop Logo"/>
+                        </a>
+                    </div>
+                    <h1>Coffee Shop</h1>
+                </div>
+
+                <div class="texto">
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil accusantium, iure voluptate tenetur animi quia fugiat dicta.
+                    </p>
+                </div>
+
+                <div class="boton">
+                    <a href="#" target="_blank">Entrar</a>
+                </div>
+
+            </div>
         </div>
     );
 }
