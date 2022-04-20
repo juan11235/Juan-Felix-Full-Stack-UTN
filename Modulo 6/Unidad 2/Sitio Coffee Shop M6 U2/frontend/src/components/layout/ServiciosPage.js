ef1d8bfda89942b126cda8b47cf7e64fd285ebd9
juan11235felix <juan11235felix@gmail.com>
2022-04-20 17:56:14 -0300

    a

---
+++ Modulo 6/Unidad 2/Sitio Coffee Shop M6 U2/frontend/src/components/layout/ServiciosPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "./Footer";
+import Nav from "./Nav";
+import Header from "./Header";
+
+const ServiciosPage = (props) => {
+    return (
+        <div>
+            <Header />
+            <Nav />
+            <Footer />
+        </div>
+    );
+}
+
+export default ServiciosPage
