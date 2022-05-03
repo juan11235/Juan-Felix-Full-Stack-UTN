a1d595b7cf2a29bd84ef42caf3431e872155c0b7
juan11235felix <juan11235felix@gmail.com>
2022-05-03 01:28:03 -0300

    m6 u3

--- Modulo 6/Unidad 3/Sitio Transportes M6 U2/backend/routes/admin/novedades.js
+++ Modulo 6/Unidad 3/Sitio Transportes M6 U2/backend/routes/admin/novedades.js
@@ -6,6 +6,7 @@ var util = require("util");
 var cloudinary = require("cloudinary").v2;
 
 const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
@@ -80,6 +81,12 @@ router.post("/agregar", async (req, res, next) => {
 
 router.get("/eliminar/:id", async (req, res, next) => {
   var id = req.params.id;
+
+  let novedad = await novedadesModel.getNovedadById(id);
+  if (novedad.ing_id) {
+    await (destroy(novedad.img_id))
+  }
+
   await novedadesModel.deleteNovedadesById(id);
   res.redirect("/admin/novedades");
 });
@@ -95,10 +102,29 @@ router.get("/modificar/:id", async (req, res, next) => {
 
 router.post("/modificar", async (req, res, next) => {
   try {
+
+    let img_id = req.body.img_original;
+    let borrar_img_vieja = false;
+    if (req.body.img_delete === '1') {
+      img_id = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+    if (borrar_img_vieja && req.body.img_original) {
+      await (destroy(req.body.img_original))
+    }
+
+
     let obj = {
       titulo: req.body.titulo,
       subtitulo: req.body.subtitulo,
       cuerpo: req.body.cuerpo,
+      img_id
     };
     //console.log(obj);
 
